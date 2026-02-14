ff59de3df2d8001919202185243c40b59407a8a8
rupeshv2121 <gp4845@myamu.ac.in>
2026-02-14 20:00:24 +0530

    CORS issue solved

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
 import { prisma } from "./config/database.js";
 import routes from "./routes/index.js";
 
 const app = express();
 const PORT = 5000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://campuscure-frontend.vercel.app",
+];
+
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Routes
