9c3c0f0b6556af9f4538ee15f5c0aea8b7797651
rupeshv2121 <gp4845@myamu.ac.in>
2026-03-07 13:57:52 +0530

    Url changes

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://campuscure-frontend.vercel.app",
+  "https://campus-cure-frontend.vercel.app",
 ].filter(Boolean) as string[];
 
 app.use(
